eaa119ce9d84c279f4b8b7be5896ab025057ed3d
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-01 02:23:57 +0100

    feat: Initialize metadata value on create and update model
    Refs #16

--- api/sermon/models/sermon.js
+++ api/sermon/models/sermon.js
@@ -5,4 +5,17 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    // Called before an entry is created
+    async beforeCreate(data) {
+      data.audio.metadata = await strapi.services.sermon.fetchMetadataFromURL(data.audio.url);
+    },
+    // Called before an entry is updated
+    async beforeUpdate(params, data) {
+      if (data.audio !== undefined) {
+        data.audio.metadata = await strapi.services.sermon.fetchMetadataFromURL(data.audio.url);
+      }
+    }
+  },
+};
